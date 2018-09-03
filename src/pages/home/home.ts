d827389f0db55764a82366e94a7cca7eff628841
Aranyak-Ghosh <32264197+Aranyak-Ghosh@users.noreply.github.com>
2018-09-04 01:04:22 +0400

    WIP Bluetooth Permissions

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,14 +1,27 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
 
+import { Diagnostics } from '@ionic-native/diagnostic'
+
+import * as Nonin3230 from '../../providers/nonin/nonin';
+
 @Component({
   selector: 'page-home',
   templateUrl: 'home.html'
 })
 export class HomePage {
 
-  constructor(public navCtrl: NavController) {
-
+  errCB = (err) => console.log(err);
+  btEnabled: boolean;
+  constructor(public navCtrl: NavController, private diagnostics: Diagnostics) {
+    this.diagnostics.isBluetoothAvailable().then(state => console.log(`Bluetooth Available? ${state}`), err => console.log(err));
+    this.diagnostics.getBluetoothState().then(state=>{
+      if(state==this.diagnostics.bluetoothState.POWERED_ON){
+        this.btEnabled=true;
+      }else{
+        this.btEnabled=false;
+      }
+    }).catch(this.errCB);
   }
 
 }
