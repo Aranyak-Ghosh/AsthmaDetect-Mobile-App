8851ac4da2ed1a77a38209420bc0ab2c051a48c5
Aranyak-Ghosh <32264197+Aranyak-Ghosh@users.noreply.github.com>
2018-09-06 02:40:22 +0400

    Completed Data Extraction from nonin

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,32 +1,38 @@
-import { Component } from "@angular/core";
+import { Component, NgZone } from "@angular/core";
 import { NavController } from "ionic-angular";
 
-import { Nonin3230Provider} from '../../providers/nonin3230/nonin3230'
+import { Nonin3230Provider } from '../../providers/nonin3230/nonin3230'
 @Component({
   selector: "page-home",
   templateUrl: "home.html"
 })
 export class HomePage {
-  msg: string;
-  val: any;
+  spo2: number;
+  heartrate: number;
   errCB = err => console.log(err);
   btEnabled: boolean;
-  constructor(public navCtrl: NavController, private nonin3230: Nonin3230Provider) {
+  constructor(public navCtrl: NavController, private nonin3230: Nonin3230Provider, private ngzone: NgZone) {
     // this.deffered()
     console.log("In Constructor");
-    
+    this.spo2=0;
+    this.heartrate=0;
   }
 
   ionViewDidLoad() {
     console.log("HomePage Loaded");
   }
 
-  scan(){
-    this.nonin3230.scanAndConnect();
+  scan() {
+    this.nonin3230.scanAndConnect().subscribe(data => {
+      this.ngzone.run(()=>{
+        this.spo2=data.spo2;
+        this.heartrate=data.pulse;
+      })
+    });
   }
 
   getData() {
     console.log('Starting Listener');
-    
+
   }
 }
--- src/providers/nonin3230/nonin3230.ts
+++ src/providers/nonin3230/nonin3230.ts
@@ -1,6 +1,8 @@
 // import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 
+import { Observable } from 'rxjs/Observable'
+
 import { BLE } from '@ionic-native/ble';
 
 /*
@@ -12,6 +14,7 @@ import { BLE } from '@ionic-native/ble';
 @Injectable()
 export class Nonin3230Provider {
 
+  data_observer: any;
   SERVICE_UUID: string = '46A970E0-0D5F-11E2-8B5E-0002A5D5C51B';
   CHAR_UUID: string = '0AAD7EA0-0D60-11E2-8E3C-0002A5D5C51B';
   constructor(private ble: BLE) {
@@ -19,24 +22,29 @@ export class Nonin3230Provider {
   }
 
   scanAndConnect() {
-    this.ble.startScan([this.SERVICE_UUID]).subscribe(peripheral => {
-      console.log(peripheral);
+    this.data_observer = Observable.create(observer => {
+      this.ble.startScan([this.SERVICE_UUID]).subscribe(peripheral => {
+        console.log(peripheral);
 
-      if (peripheral && peripheral.name && peripheral.name.indexOf('Nonin3230_') > -1 && peripheral.id) {
-        this.ble.stopScan();
-        this.ble.connect(peripheral.id).subscribe(status=>{
-          console.log(status);
-          this.ble.startNotification(status.id,this.SERVICE_UUID,this.CHAR_UUID).subscribe(data=>{
-            console.log(data);
-            
-            let temp=new Uint8Array(data);
-            let spo2=temp[7];
-            let pulse=temp[8]*256+temp[9];
-            debugger;
+        if (peripheral && peripheral.name && peripheral.name.indexOf('Nonin3230_') > -1 && peripheral.id) {
+          this.ble.stopScan();
+          this.ble.connect(peripheral.id).subscribe(status => {
+            console.log(status);
+            this.ble.startNotification(status.id, this.SERVICE_UUID, this.CHAR_UUID).subscribe(data => {
+              console.log(data);
+
+              let temp = new Uint8Array(data);
+              let spo2 = temp[7];
+              let pulse = temp[8] * 256 + temp[9];
+              observer.next({ spo2, pulse });
+            });
           })
-        })
-      }
+        }
+      });
     })
+
+
+    return this.data_observer;
   }
 
 
