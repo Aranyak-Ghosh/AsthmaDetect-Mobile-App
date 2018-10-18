73ecc2c4a60e269c65577c0c3af2372faedc0947
Aranyak-Ghosh <32264197+Aranyak-Ghosh@users.noreply.github.com>
2018-10-18 10:02:09 +0400

    Figured out issue with charts

--- src/pages/sleep/sleep.ts
+++ src/pages/sleep/sleep.ts
@@ -40,48 +40,59 @@ export class SleepPage {
   }
 
   async submit() {
-    let data = await this.fitbit.getSleep(
-      this.date.toISOString().split("T")[0]
-    );
+    // let data = await this.fitbit.getSleep(
+    //   this.date.toISOString().split("T")[0]
+    // );
     // console.log(data);
+
+    // this.inner =
+    //   '<img style="max-height:100%;height:auto;width:100%;max-width:100%;margin:auto;display:inline;" src="' +
+    //   this.graph.toBase64Image() +
+    //   '" />';
+    // $("#sleepChart")
+    //   .parent()
+    //   .html(this.inner);
+
     this.graph = new Chart(this.sleepChart.nativeElement, {
- 
-      type: 'bar',
+      type: "bar",
       data: {
-          labels: ["Red", "Blue", "Yellow", "Green", "Purple", "Orange"],
-          datasets: [{
-              label: '# of Votes',
-              data: [12, 19, 3, 5, 2, 3],
-              backgroundColor: [
-                  'rgba(255, 99, 132, 0.2)',
-                  'rgba(54, 162, 235, 0.2)',
-                  'rgba(255, 206, 86, 0.2)',
-                  'rgba(75, 192, 192, 0.2)',
-                  'rgba(153, 102, 255, 0.2)',
-                  'rgba(255, 159, 64, 0.2)'
-              ],
-              borderColor: [
-                  'rgba(255,99,132,1)',
-                  'rgba(54, 162, 235, 1)',
-                  'rgba(255, 206, 86, 1)',
-                  'rgba(75, 192, 192, 1)',
-                  'rgba(153, 102, 255, 1)',
-                  'rgba(255, 159, 64, 1)'
-              ],
-              borderWidth: 1
-          }]
+        labels: ["Red", "Blue", "Yellow", "Green", "Purple", "Orange"],
+        datasets: [
+          {
+            label: "# of Votes",
+            data: [12, 19, 3, 5, 2, 3],
+            backgroundColor: [
+              "rgba(255, 99, 132, 0.2)",
+              "rgba(54, 162, 235, 0.2)",
+              "rgba(255, 206, 86, 0.2)",
+              "rgba(75, 192, 192, 0.2)",
+              "rgba(153, 102, 255, 0.2)",
+              "rgba(255, 159, 64, 0.2)"
+            ],
+            borderColor: [
+              "rgba(255,99,132,1)",
+              "rgba(54, 162, 235, 1)",
+              "rgba(255, 206, 86, 1)",
+              "rgba(75, 192, 192, 1)",
+              "rgba(153, 102, 255, 1)",
+              "rgba(255, 159, 64, 1)"
+            ],
+            borderWidth: 1
+          }
+        ]
       },
       options: {
-          scales: {
-              yAxes: [{
-                  ticks: {
-                      beginAtZero:true
-                  }
-              }]
-          }
+        scales: {
+          yAxes: [
+            {
+              ticks: {
+                beginAtZero: true
+              }
+            }
+          ]
+        }
       }
-
-  });
+    });
   }
 
   ionViewDidLoad() {
