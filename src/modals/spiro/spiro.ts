12aad72027e2774a002746dd12770f98755ce29d
Aranyak-Ghosh <32264197+Aranyak-Ghosh@users.noreply.github.com>
2018-11-22 19:15:58 +0400

    Updated form for asthma and COPD

--- src/modals/spiro/spiro.ts
+++ src/modals/spiro/spiro.ts
@@ -8,21 +8,79 @@ import { VitalProvider } from "../../providers/vital/vital";
 })
 export class ModalSpiroPage {
   loadingMeta: boolean = true;
+  text: String;
+  copd: boolean;
+  question: any;
   record = {
     fev1: null,
     fvc: null,
-    pef: null
+    pef: null,
+    fef2575: null
   };
+  copdQ: any = [
+    {
+      placeholder: "FEV1",
+      name: "fev1",
+      model: "record.fev1"
+    },
+    {
+      placeholder: "FVC",
+      name: "fvc",
+      model: "record.fvc"
+    },
+    {
+      placeholder: "PEF",
+      name: "pef",
+      model: "record.pef"
+    }
+  ];
+
+  asthmaQ: any = [
+    {
+      placeholder: "FEV1",
+      name: "fev1",
+      model: "record.fev1"
+    },
+    {
+      placeholder: "FVC",
+      name: "fvc",
+      model: "record.fvc"
+    },
+    {
+      placeholder: "PEF",
+      name: "pef",
+      model: "record.pef"
+    },
+    {
+      placeholder: "FEF 25-75",
+      name: "fef2575",
+      model: "record.fef2575"
+    }
+  ];
 
   constructor(
     public navCtrl: NavController,
     public navParams: NavParams,
     private viewCtrl: ViewController,
     private vital: VitalProvider
-  ) {}
+  ) {
+    this.copd = false;
+    this.text = "Ashtma";
+    this.question = this.asthmaQ;
+  }
 
   dismiss() {
     this.viewCtrl.dismiss();
+  }
+
+  toggleClicked() {
+    if (this.copd) {
+      this.text = "COPD";
+      this.question = this.copdQ;
+    } else {
+      this.text = "Asthma";
+      this.question = this.asthmaQ;
+    }
   }
 
   async submit() {
