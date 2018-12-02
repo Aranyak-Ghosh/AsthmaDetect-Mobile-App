7f46391e430aedd25d594fa9a653ed7087564b7d
Aranyak-Ghosh <32264197+Aranyak-Ghosh@users.noreply.github.com>
2018-12-02 19:36:03 +0400

    Adding option to send COPD vals

--- src/pages/spirometry/spirometry.ts
+++ src/pages/spirometry/spirometry.ts
@@ -1,7 +1,12 @@
 import { Component } from "@angular/core";
-import { NavController, NavParams, ModalController, Modal } from "ionic-angular";
+import {
+  NavController,
+  NavParams,
+  ModalController,
+  Modal
+} from "ionic-angular";
 
-import {ModalSpiroPage} from "../../modals/spiro/spiro"
+import { ModalSpiroPage } from "../../modals/spiro/spiro";
 /**
  * Generated class for the SpirometryPage page.
  *
@@ -14,13 +19,28 @@ import {ModalSpiroPage} from "../../modals/spiro/spiro"
   templateUrl: "spirometry.html"
 })
 export class SpirometryPage {
-  constructor(public navCtrl: NavController, public navParams: NavParams, public modalCtrl:ModalController) {}
+  date: any;
+  type: String = "string";
+  filterSelected: boolean = false;
+  constructor(
+    public navCtrl: NavController,
+    public navParams: NavParams,
+    public modalCtrl: ModalController
+  ) {}
 
   ionViewDidLoad() {
     console.log("ionViewDidLoad SpirometryPage");
   }
 
-  showModal(){
+  toggleFilter() {
+    this.filterSelected = !this.filterSelected;
+  }
+
+  submit() {
+    //TODO: Implement sending date and receiving list of all values
+  }
+
+  showModal() {
     this.modalCtrl.create(ModalSpiroPage).present();
   }
 }
