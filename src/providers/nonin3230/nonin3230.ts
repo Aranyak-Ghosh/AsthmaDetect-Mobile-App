01662189e90f25deb6f437d59ff8c8d7e567d430
Aranyak-Ghosh <32264197+Aranyak-Ghosh@users.noreply.github.com>
2018-12-02 19:36:16 +0400

    Error handling for oximeter

--- src/providers/nonin3230/nonin3230.ts
+++ src/providers/nonin3230/nonin3230.ts
@@ -1,9 +1,9 @@
 // import { HttpClient } from '@angular/common/http';
-import { Injectable } from '@angular/core';
+import { Injectable } from "@angular/core";
 
-import { Observable } from 'rxjs/Observable'
+import { Observable } from "rxjs/Observable";
 
-import { BLE } from '@ionic-native/ble';
+import { BLE } from "@ionic-native/ble";
 
 /*
   Generated class for the Nonin3230Provider provider.
@@ -13,12 +13,11 @@ import { BLE } from '@ionic-native/ble';
 */
 @Injectable()
 export class Nonin3230Provider {
-
   data_observer: any;
-  SERVICE_UUID: string = '46A970E0-0D5F-11E2-8B5E-0002A5D5C51B';
-  CHAR_UUID: string = '0AAD7EA0-0D60-11E2-8E3C-0002A5D5C51B';
+  SERVICE_UUID: string = "46A970E0-0D5F-11E2-8B5E-0002A5D5C51B";
+  CHAR_UUID: string = "0AAD7EA0-0D60-11E2-8E3C-0002A5D5C51B";
   constructor(private ble: BLE) {
-    console.log('Hello Nonin3230Provider Provider');
+    console.log("Hello Nonin3230Provider Provider");
   }
 
   scanAndConnect() {
@@ -26,26 +25,30 @@ export class Nonin3230Provider {
       this.ble.startScan([this.SERVICE_UUID]).subscribe(peripheral => {
         console.log(peripheral);
 
-        if (peripheral && peripheral.name && peripheral.name.indexOf('Nonin3230_') > -1 && peripheral.id) {
+        if (
+          peripheral &&
+          peripheral.name &&
+          peripheral.name.indexOf("Nonin3230_") > -1 &&
+          peripheral.id
+        ) {
           this.ble.stopScan();
           this.ble.connect(peripheral.id).subscribe(status => {
             console.log(status);
-            this.ble.startNotification(status.id, this.SERVICE_UUID, this.CHAR_UUID).subscribe(data => {
-              console.log(data);
+            this.ble
+              .startNotification(status.id, this.SERVICE_UUID, this.CHAR_UUID)
+              .subscribe(data => {
+                console.log(data);
 
-              let temp = new Uint8Array(data);
-              let spo2 = temp[7];
-              let pulse = temp[8] * 256 + temp[9];
-              observer.next({ spo2, pulse });
-            });
-          })
+                let temp = new Uint8Array(data);
+                let spo2 = temp[7];
+                let pulse = temp[8] * 256 + temp[9];
+                if (pulse < 500) observer.next({ spo2, pulse });
+              });
+          });
         }
       });
-    })
-
+    });
 
     return this.data_observer;
   }
-
-
 }
